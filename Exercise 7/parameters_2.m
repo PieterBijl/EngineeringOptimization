ccd5aca5fd72711133c59935d3fb48930b5f1d6c
PieterBijl <pieterbijl1@gmail.com>
2020-06-28 16:32:16 +0200

    added new path calculator and parameters in matlab

---
+++ Exercise 7/parameters_2.m
@@ -0,0 +1,7 @@
+w_euro = 0.5;
+w_CO2 = 0.5;
+fuel_cost = [0.1; 0; 0.05];
+maintenance_cost = [0.1; 0.3; 0.05];
+build_cost = [0.1; 0.2; 0.1]; % How much to build for coal and solar
+CO2_cost = [0.2; 0.05; 0.1]; % How much CO2 do they emit?
+budget = 1;
