7674c90521d18f6b45a8568c9672e5fa39071e18
PieterBijl <pieterbijl1@gmail.com>
2020-06-29 22:54:25 +0200

    added new type of market function

---
+++ Sandbox Pieter/V1/build_cost.m
@@ -0,0 +1,3 @@
+function build_cost = build_cost(x)
+    build_cost(1) = build_constant(1)*x(1)
+end
--- Sandbox Pieter/V1/runfmincon.m
+++ Sandbox Pieter/V1/runfmincon.m
@@ -6,10 +6,10 @@ history.fval = [];
 searchdir = [];
  
 % call optimization
-A = [1, 1, 1]; b = 1;
-x0 = [1; 0; 0];
-Aeq = [1, 1, 1]; beq = 1;
-lb = [0 0 0]; ub = [1 1 1];
+A = [1, 1, 1, 1]; b = 1;
+x0 = [1; 0; 0; 0];
+Aeq = [1, 1, 1, 1]; beq = 1;
+lb = [0 0 0 0]; ub = [1 1 1 1];
 nonlcon = [];
 options = optimoptions(@fmincon,'OutputFcn',@outfun,... 
     'Display','iter','Algorithm','sqp');
@@ -43,11 +43,12 @@ xsol = fmincon(@objfun,x0,A,b,Aeq,beq,lb,ub,nonlcon,options);
  end
  
  function f = objfun(x)
-    w_euro = 0.5;
-    w_CO2 = 0.5;
-    fuel_cost = [0.1; 0];
-    maintenance_cost = [0.1; 0.3];
-    CO2_cost = [0.2; 0.05]; % How much CO2 do they emit?
-    f = sum([x(1); x(2)].*(w_euro*(maintenance_cost+fuel_cost)+w_CO2*CO2_cost));
+    w_euro = 0;
+    w_CO2 = 1;
+    fuel_cost = [0.3; 0; 0.15; 0];
+    maintenance_cost = [0.1; 0.05; 0.15; 0.2];
+    misc_cost = [0.55*x(1)^2-1.1*x(1)+0.6; 5.2*x(2)^2-5.2*x(2)+1.5; 0.55*x(1)^2-1.1*x(1)+0.6; 1.8*x(4)^2-1.5*x(4)+0.5];
+    CO2_cost = [0.3; 0.0; 0.2; 0.0]; % How much CO2 do they emit?
+    f = sum([x(1); x(2); x(3); x(4)].*(w_euro*(maintenance_cost+fuel_cost+misc_cost)+w_CO2*CO2_cost));
  end
 end
