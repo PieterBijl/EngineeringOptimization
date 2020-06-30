cd765a47feddf9376e74c4b9263d504f8458c061
PieterBijl <pieterbijl1@gmail.com>
2020-06-30 11:21:27 +0200

    changed some folders

--- Sandbox Pieter/V2 market/market_force.m
+++ Sandbox Pieter/V2 market/market_force.m
@@ -1,5 +1,5 @@
 clear all
-powerplant_parameters;
+powerplant_parameters_2;
 A = [1, 1, 1, 1]; b = 1;
 x0 = [1; 0; 0; 0];
 Aeq = [1, 1, 1, 1]; beq = 1;
@@ -11,7 +11,7 @@ i = 1;
 gov_total_cost(1) = 0;
 total_market_cost(1) = 0;
 tic;
-while sum(abs(x1(:,i)-xsol)) > 0.01 & i<1000
+while sum(abs(x1(:,i)-xsol)) > 0.05 & i<1000
     options = optimoptions(@fmincon,'MaxIterations',1,'Algorithm','interior-point','Display','off');
     [xsol_temp,f1(i)] = fmincon(@objfun,x1(:,i),A,b,Aeq,beq,lb,ub,nonlcon,options);
     dx = xsol_temp-x1(:,i);
@@ -20,12 +20,12 @@ while sum(abs(x1(:,i)-xsol)) > 0.01 & i<1000
     dx_step = budget/unit_cost*norm_dx;
     x1(:,i+1) = x1(:,i)+dx_step;
     misc_cost = [0.55*x1(1,i)^2-1.1*x1(1,i)+0.6; 5.2*x1(2,i)^2-5.2*x1(2,i)+1.5; 0.55*x1(1,i)^2-1.1*x1(1,i)+0.6; 1.8*x1(4,i)^2-1.5*x1(4,i)+0.5];
-    market_cost(:,i) = x1(:,i).*budget*(maintenance_cost+energy_cost+misc_cost+1/20*(build_cost+build_subsidies)+subsidies);
+    market_cost(:,i) = x1(:,i)*budget.*((energy_cost+1/capital_loan_duration*(build_cost+build_subsidies)).*misc_cost+subsidies);
     total_market_cost(i+1) = total_market_cost(i)+sum(market_cost(:,i));
     gov_build_subsidies(:,i) = abs(dx_step).*abs(build_subsidies);
     gov_subsidies(:,i) = budget*x1(:,i).*subsidies;
     gov_cost(:,i) = gov_build_subsidies(:,i)+gov_subsidies(:,i);
-    gov_total_cost(i+1) = total_cost(i)+sum(gov_cost(:,i));
+    gov_total_cost(i+1) = gov_total_cost(i)+sum(gov_cost(:,i));
     i = i + 1;
 end
 toc;
@@ -37,8 +37,8 @@ ylabel('Gas')
 zlabel('Wind')
 
 function f = objfun(x)
-    powerplant_parameters;
+    powerplant_parameters_2;
     misc_cost = [0.55*x(1)^2-1.1*x(1)+0.6; 5.2*x(2)^2-5.2*x(2)+1.5; 0.55*x(1)^2-1.1*x(1)+0.6; 1.8*x(4)^2-1.5*x(4)+0.5];
-    f = sum([x(1); x(2); x(3); x(4)].*(w_euro*(maintenance_cost+energy_cost+misc_cost+1/20*(build_cost+build_subsidies)+subsidies)+w_CO2*CO2_cost));
+    f = sum([x(1); x(2); x(3); x(4)].*(w_euro*(energy_cost+1/capital_loan_duration*(build_cost+build_subsidies).*misc_cost+subsidies)+w_CO2*CO2_cost));
 end
 
