e09e2eceed50c79735b33d883ba06f9cf3f20b28
PieterBijl <pieterbijl1@gmail.com>
2020-07-01 19:52:52 +0200

    newest version of GA

--- Sandbox Pieter/GA/V1/scenario.m
+++ Sandbox Pieter/GA/V1/scenario.m
@@ -1,5 +1,5 @@
 function f = scenario(sub)
-    global x0 energy_cost capital_loan_duration build_cost CO2_cost P subsidies build_subsidies
+    global x0 energy_cost capital_loan_duration build_cost CO2_cost P subsidies build_subsidies plot_on w_CO2
     budget = 10^9/P; % budget per year in dollars
     t_end = 30*8760; % Time after which the simulation ends in hours
 %     subsidies = subsidies;
@@ -19,6 +19,7 @@ function f = scenario(sub)
     gov_total_cost(1) = 0;
     total_market_cost(1) = 0;
     CO2_total_cost(1) = 0;
+    tax_factor = 0;
     t = 0;
     time_step = 1000; % Timestep in hours
 while t<t_end
@@ -30,21 +31,24 @@ while t<t_end
     M_f = [1; 1; 1; 4*x1(4,i)^2-4*x1(4,i)+2; 1];
     market_cost(:,i) = time_step*x1(:,i).*(M_f.*((build_cost+build_subsidies')/(capital_loan_duration*8760)+energy_cost)+subsidies');
     total_market_cost(i+1) = total_market_cost(i)+P*sum(market_cost(:,i));
-    gov_build_subsidies(:,i) = P*abs(dx_step).*abs(build_subsidies');
-    gov_subsidies(:,i) = P*budget*x1(:,i).*subsidies';
-    gov_cost(:,i) = gov_build_subsidies(:,i)+gov_subsidies(:,i);
-    gov_total_cost(i+1) = gov_total_cost(i)+abs(sum(gov_cost(:,i)));
-    added_CO2(:,i) = P*x1(:,i).*CO2_cost;
+    gov_build_subsidies(:,i) = -P*abs(dx_step).*build_subsidies';
+    gov_subsidies(:,i) = -P*time_step*x1(:,i).*subsidies';
+    gov_cost(:,i) = gov_build_subsidies(:,i)+gov_subsidies(:,i)+tax_factor*(abs(gov_subsidies(:,i))+abs(gov_build_subsidies(:,i)));
+    gov_total_cost(i+1) = gov_total_cost(i)+sum(gov_cost(:,i));
+    added_CO2(:,i) = time_step*P*x1(:,i).*CO2_cost;
     CO2_total_cost(i+1) = CO2_total_cost(i)+sum(added_CO2(:,i));
     i = i + 1;
     t = t + time_step;
 end
-
-%           n = ["Coal"; "Gas"; "Wind"; "Solar";"Nuclear"];
-%           PlotStates(n,x1,24)
-%       figure;
-%       plot(f1)
-    social_cost = 50/1000;
-    f = gov_total_cost(end) + social_cost*CO2_total_cost(end);
-%     f = gov_total_cost(end);
+    f = gov_total_cost(end) + w_CO2*CO2_total_cost(end);
+    if plot_on == 1
+       n = ["Coal"; "Gas"; "On-shore Wind"; "Solar";"Nuclear"];
+       PlotStates(n,x1,24)
+       disp("Costs in dollars for the government")
+       gov_total_cost(end)
+       disp("CO2 in dollars")
+       w_CO2*CO2_total_cost(end)
+    end
+%        figure;
+%        plot(f1)
 end
