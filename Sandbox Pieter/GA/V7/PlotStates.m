5b492c075c13aeb224e6116d31d621a9249f2540
PieterBijl <pieterbijl1@gmail.com>
2020-07-02 15:57:49 +0200

    fixed plotstates

--- Sandbox Pieter/GA/V7/PlotStates.m
+++ Sandbox Pieter/GA/V7/PlotStates.m
@@ -5,7 +5,10 @@ endDate = datetime('today')+hours(timestep*length(states'));
 dates = linspace(startDate, endDate, length(states'));
 
 figure
-area(dates, states')
+h = area(dates, states');
+for i = 1:length(names(:,2))
+    h(i).FaceColor = cell2mat(names(i,2));
+end
 datetick('x','yyyy','keepticks')
 xlabel("Time")
 ylim([0 1])
@@ -13,11 +16,10 @@ ylabel("Fraction of total power supply")
 
 % Set location and size of figure
 x0=50;
-y0=300;
+y0=50;
 width=700;
 height=400;
 set(gcf,'position',[x0,y0,width,height])
 
-legend(names(:,1),'Location', 'eastoutside')
-
+legend(names(:,1), 'Location', 'eastoutside')
 end
